009059cd8506645ebc78c8e3b5707caefce95a29
= <=>
2021-04-01 08:48:58 +0700

    update roomlist

--- js/home.js
+++ js/home.js
@@ -3,39 +3,104 @@ function loadRoomlist(id) {
     child.innerHTML = '<h1>Loading...</h1>';
     setTimeout(() => {
         child.innerHTML = '<h1>Success...</h1>';
-        $('#child').load("roomlistView.html #load", function(){            
+        $('#child').load("roomlistView.html #load", function () {
             $('#extable').DataTable();
-        });                
+            $('[data-toggle="tooltip"]').tooltip();
+
+            // Select/Deselect checkboxes
+            var checkbox = $('table tbody input[type="checkbox"]');
+            $("#selectAll").click(function () {
+                if (this.checked) {
+                    checkbox.each(function () {
+                        this.checked = true;
+                    });
+                }
+                else {
+                    checkbox.each(function () {
+                        this.checked = false;
+                    });
+                }
+            });
+            checkbox.click(function () {
+                if (!this.checked) {
+                    $("#selectAll").prop("checked", false);
+                }
+            });
+        });
     }, 500);
-        
+
 }
-
-
 
 function loadBooked(id) {
     var child = document.getElementById("child");
     child.innerHTML = '<h1>Loading...</h1>';
     setTimeout(() => {
         child.innerHTML = '<h1>Success...</h1>';
-        $('#child').load("bookedView.html #load");
+        $('#child').load("bookedView.html #load", function () {
+            $('#bookedTable').DataTable();
+            $('[data-toggle="tooltip"]').tooltip();
+            // Select/Deselect checkboxes
+            var checkbox = $('table tbody input[type="checkbox"]');
+            $("#selectAll").click(function () {
+                if (this.checked) {
+                    checkbox.each(function () {
+                        this.checked = true;
+                    });
+                }
+                else {
+                    checkbox.each(function () {
+                        this.checked = false;
+                    });
+                }
+            });
+            checkbox.click(function () {
+                if (!this.checked) {
+                    $("#selectAll").prop("checked", false);
+                }
+            });
+        });
     }, 500);
 }
-// function loadCheckin(id) {
-//     var child = document.getElementById("child");
-//     child.innerHTML = '<h1>Loading...</h1>';
-//     setTimeout(() => {
-//         child.innerHTML = '<h1>Success...</h1>';
-//         $('#child').load("checkinView.html #load");
-//     }, 500);
-// }
-// function loadCheckout(id) {
-//     var child = document.getElementById("child");
-//     child.innerHTML = '<h1>Loading...</h1>';
-//     setTimeout(() => {
-//         child.innerHTML = '<h1>Success...</h1>';
-//         $('#child').load("checkoutView.html #load");
-//     }, 500);
-// }
+function loadCheckin(id) {
+    var child = document.getElementById("child");
+    child.innerHTML = '<h1>Loading...</h1>';
+    setTimeout(() => {
+        child.innerHTML = '<h1>Success...</h1>';
+        $('#child').load("checkinView.html #load", function () {
+            $('#checkinTable').DataTable();
+            $('[data-toggle="tooltip"]').tooltip();
+            // Select/Deselect checkboxes
+            var checkbox = $('table tbody input[type="checkbox"]');
+            $("#selectAll").click(function () {
+                if (this.checked) {
+                    checkbox.each(function () {
+                        this.checked = true;
+                    });
+                }
+                else {
+                    checkbox.each(function () {
+                        this.checked = false;
+                    });
+                }
+            });
+            checkbox.click(function () {
+                if (!this.checked) {
+                    $("#selectAll").prop("checked", false);
+                }
+            });
+        });
+    }, 500);
+}
+function loadCheckout(id) {
+    var child = document.getElementById("child");
+    child.innerHTML = '<h1>Loading...</h1>';
+    setTimeout(() => {
+        child.innerHTML = '<h1>Success...</h1>';
+        $('#child').load("checkoutView.html #load", function () {
+            $('#checkoutTable').DataTable();
+        });
+    }, 500);
+}
 // function loadService(id) {
 //     var child = document.getElementById("child");
 //     child.innerHTML = '<h1>Loading...</h1>';
